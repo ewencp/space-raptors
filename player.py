f1c16546a3fd76b074bfd5f11ef027b04c978906
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-11-16 16:19:40 -0800

    Add simple game support.
    Add game support to the protocol and GUI. Users can create games,
    lists of open games are pushed to clients and displayed, another user
    can try to join the game, and both parties are notified when a match
    is made.

--- player.py
+++ player.py
@@ -11,6 +11,23 @@ from pgu import gui
 
 import time
 
+from Queue import Queue
+
+
+handlers = Queue()
+def threaded(fn):
+    def handler(*args, **kwargs):
+        handlers.put( (fn, args, kwargs) )
+    return handler
+
+def run_handlers():
+    # Safe to check empty and assume we'll get something because we're
+    # the only consumer
+    while not handlers.empty():
+        fn, args, kwargs = handlers.get()
+        fn(*args, **kwargs)
+
+
 class SessionState(object):
     '''Pure state abstraction for the GUI'''
     def __init__(self, net):
@@ -32,7 +49,7 @@ class LobbyGUI(object):
         self.app = gui.Desktop()
         self.app.connect(gui.QUIT, self._handle_quit, None)
 
-        self.layout = gui.Table(width=600, height=120)
+        self.layout = gui.Table(width=600, height=600)
 
         self.layout.tr()
 
@@ -50,8 +67,23 @@ class LobbyGUI(object):
         self.quit_button.connect(gui.CLICK, self._handle_quit, None)
         self.layout.td(self.quit_button)
 
+
+        self.create_game_button = gui.Button("Create New Game")
+        self.create_game_button.connect(gui.CLICK, self._handle_create_game, None)
+
+        self.games_list = gui.Table()
+
     def run(self):
-        self.app.run(self.layout)
+        # Control manually so we can setup timers / process async
+        # events from Waldo
+        self.app.init(self.layout)
+        while not self.app._quit:
+            # GUI Loop
+            self.app.loop()
+            # Event handlers
+            run_handlers()
+            # Wait for next iteration
+            pygame.time.wait(10)
 
     def set_username(self, value):
         self.login_username.value = value
@@ -62,17 +94,26 @@ class LobbyGUI(object):
             self.login_button.value = 'Change Username'
             self.set_status(True)
 
-    def set_status(self, status, timeout=None):
+            # Add game listing info and control buttons
+            self.layout.tr()
+            self.layout.td(self.create_game_button)
+            self.layout.td(self.games_list, colspan=3)
+
+    def set_status(self, status):
         img_src = (status and 'data/check_24.png') or 'data/delete_24.png'
-        print 'loading', img_src, status
         img = pygame.image.load(img_src)
-        print img
         if img:
             self.connected_status.value = img
             self.app.repaint()
 
-        if timeout is not None:
-            pass
+    def set_games_list(self, games):
+        self.games_list.clear()
+        for g in games:
+            self.games_list.tr()
+            self.games_list.td(gui.Label(str(g)))
+            join_button = gui.Button("Join")
+            join_button.connect(gui.CLICK, self._handle_join, g)
+            self.games_list.td(join_button)
 
     def _handle_login(self, value=None):
         username = self.login_username.value
@@ -84,14 +125,23 @@ class LobbyGUI(object):
         else:
             self.listener.on_change_username(username)
 
+    def _handle_create_game(self, value=None):
+        self.listener.on_create_game()
+
+    def _handle_join(self, game):
+        self.listener.on_join(game)
+
     def _handle_quit(self, value=None):
         self.app.quit(value)
+
 
 class Controller(object):
     '''Coordinates the GUI and the networking code'''
 
     def __init__(self):
         self.net = Waldo.connect(
+            self.on_updated_open_game_list,
+            self.on_matched_game,
             constructor = Client,
             host_name = config.host,
             port = config.port
@@ -106,6 +156,7 @@ class Controller(object):
         self.net.begin_session(username, self.on_began_session)
 
     def on_began_session(self):
+        print "on_began_session"
         # Forward any possible change name back to the GUI
         self.gui.set_username(self.net.get_username())
         self.gui.set_connected(True)
@@ -114,10 +165,39 @@ class Controller(object):
         self.net.change_username(username, self.on_change_username_response)
 
     def on_change_username_response(self, changed):
+        print "on_change_username_response"
         # Either way, make sure we reflect the current state
         self.gui.set_username(self.net.get_username())
-        print changed
         self.gui.set_status(changed)
+
+    def on_create_game(self):
+        self.net.create_new_game()
+
+    @threaded
+    def on_updated_open_game_list(self, games):
+        print "on_updated_open_game_list"
+        self.gui.set_games_list(games)
+
+    def on_join(self, game):
+        # Try to join the game. We need to use a callback because we
+        # can't get return values here. We need succeeded to be a list
+        # because we need to set it in the callback
+        succeeded = []
+        def on_join_game_finished(suc):
+            succeeded.append(suc)
+
+        self.net.join_game(game, on_join_game_finished)
+        # Block until we're sure the callback got invoked
+        while len(succeeded) == 0: pass
+        if succeeded[0]:
+            print "Joined game", game
+        else:
+            print "Failed to join game", game
+
+    @threaded
+    def on_matched_game(self, guest):
+        print "on_matched_game"
+        print "  ", guest
 
 def main():
     Waldo.initialize()
--- server.py
+++ server.py
@@ -8,18 +8,50 @@ import config
 class LobbyServer:
 
     def __init__(self):
+        self._connections = []
+
         self._users = Waldo.ExternalList([])
+        self._matching_users = Waldo.ExternalList([])
+        self._open_games = Waldo.ExternalList([])
+        self._active_games = Waldo.ExternalList([])
         Waldo.accept(
             self._users,
+            self._matching_users,
+            self._open_games,
+            self._active_games,
+            self.on_open_game_added,
+            self.on_matched_game,
             constructor = Lobby,
             host_name = config.host,
             port = config.port,
             connected_callback=self.on_connection
             )
 
-    def on_connection(self, client):
+    def on_connection(self, conn):
         print "Received client connection"
+        self._connections.append(conn)
 
+    def on_open_game_added(self):
+        '''When a new game is opened (advertised) by a user, we need
+        to notify other connections about the new game in case they
+        are still looking for games. The Waldo code will filter out
+        ones that don't need to see the change (they are already
+        matched and in a game)'''
+        for conn in self._connections:
+            conn.push_game_list()
+
+    def on_matched_game(self, owner, guest):
+        print "Matched game between %s and %s" % (owner, guest)
+        print "Pushing updated game lists for all clients"
+        for conn in self._connections:
+            conn.push_game_list()
+            # Notify the owner that they're in a game now.
+            # It sucks that we have to do this for everyone, but we'll
+            # only send anything to the owner (everyone else gets
+            # filtered before hitting a sequence). We can't find the
+            # right person since only have connection objects in
+            # Python-land.
+            conn.notify_owner_of_guest(owner, guest)
 
 def main():
     Waldo.initialize()
